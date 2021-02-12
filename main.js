e6479726b3f05c139c9190d3e098f8e8a19e255d
MorganeK11 <63037684+MorganeK11@users.noreply.github.com>
2021-02-12 16:38:16 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+//Get the button
+
+var mybutton = document.getElementById("myBtn");
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    mybutton.style.display = "block";
+  } else {
+    navbar.classList.remove("sticky");
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
